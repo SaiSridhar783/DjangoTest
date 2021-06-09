c4015fae96b82a93899a16cb05eaf3e3132bdbcc
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-09 12:55:18 +0530

    Registering app in settings.py

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,7 @@
-import challenges
 from django.shortcuts import render
-from django.http.response import HttpResponseRedirect
-from django.http import HttpResponse, HttpResponseNotFound
-from django.urls.base import reverse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Learn 1 new Skill, to near perfection",
@@ -48,7 +47,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>Bankai {challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
 
     except Exception as e:
