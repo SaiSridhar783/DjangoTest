e57a26311f86aa2bcfdd24c2e44a3e2d881e71aa
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-09 12:00:18 +0530

    Views and URL (dynamic)

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,22 @@
+import challenges
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Learn 1 new Skill, to near perfection"
+
+    elif month == "february":
+        challenge_text = "Walk 20 minutes everyday, minimum."
+
+    elif month == "march":
+        challenge_text = "Practice Django for 30 mins everyday."
+
+    else:
+        return HttpResponseNotFound("This month is not in existence!!")
+
+    return HttpResponse(challenge_text)
