eb9857a6e24be0d29a597e5eaed99627a92d0eac
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-09 13:31:58 +0530

    Django Tags used {% %} (for, if, url)

--- challenges/views.py
+++ challenges/views.py
@@ -14,22 +14,18 @@ monthly_challenges = {
     "september": "Exercise 40 mins everyday",
     "october": "Hacktober",
     "november": "No Shave November",
-    "december": "Lazy to do anything actually"
+    "december": None
 }
 
 # Create your views here.
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href='{month_path}'>{month.capitalize()}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
