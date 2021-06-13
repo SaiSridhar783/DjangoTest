8158e01f09406b875697cf0b94519ef1d1fd4873
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-13 13:12:56 +0530

    Aggregate methods and Order By

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+#from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("rating", "-title",)
+
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
 
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_books,
+        "avg_rating": avg_rating
     })
 
 
@@ -25,5 +31,5 @@ def book_detail(request, slug):
         "title": book.title,
         "author": book.author,
         "rating": book.rating,
-        "bestseller": book.is_bestselling
+        "is_bestseller": book.is_bestselling
     })
