7ee468274cac05d8b871eb17e291bfc5c9e14142
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-15 23:18:16 +0530

    Simplifying Upload View

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -13,22 +14,8 @@ from .models import UserProfile
             dest.write(chunk) '''
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create+profile.html", {
-            "form": submitted_form
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
