6ec1f096c10e1109828fff64abc0252b2f8e1a1c
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-15 16:09:58 +0530

    Using ModelForm in Views

--- reviews/views.py
+++ reviews/views.py
@@ -9,12 +9,11 @@ from .models import Review
 
 def review(request):
     if request.method == "POST":
-        form = ReviewForm(request.POST)
+        existing_model = Review.objects.get(pk=1)
+        form = ReviewForm(request.POST, instance=existing_model)
 
         if form.is_valid():
-            review = Review(user_name=form.cleaned_data['user_name'],
-                            review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
-            review.save()
+            form.save()
 
             return HttpResponseRedirect("/thank-you")
 
