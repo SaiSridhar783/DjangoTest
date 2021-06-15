06ffc98ad752e9fd46327025439952ff1e68e669
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-15 23:27:59 +0530

    Serving Uploaded Files

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -19,3 +20,9 @@ class CreateProfileView(CreateView):
     model = UserProfile
     fields = "__all__"
     success_url = "/profiles"
+
+
+class UserProfileView(ListView):
+    template_name = "profiles/user_profile.html"
+    model = UserProfile
+    context_object_name = "profiles"
